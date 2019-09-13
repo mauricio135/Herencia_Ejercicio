4de8e6e56e1eb86ad48397cac8937bb7ca475550
fedemartino <fedemartino@gmail.com>
2019-09-13 10:57:03 -0300

    pseudocodigo actualizado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -12,33 +12,28 @@ namespace PII_Herencia
             planteamos un ejemplo de como hacerlo. Esto no significa que te limites a hacer solamente esto, debes pensar 
             en grande!
 
-            Pasajero pasajero1 = nuevo Pasajero()
-            Pasajero pasajero2 = nuevo Pasajero()
-            Pasajero pasajero3 = nuevo Pasajero()
-            Conductor conductor1 = nuevo Conductor()
-            ConductorPool conductorPool1 = nuevo ConductorPool()
-            conductorPool1.MaxPasajeros = 3
+            Usuario pasajero1 = nuevo Pasajero()
+            Usuario pasajero2 = nuevo Pasajero()
+            Usuario pasajero3 = nuevo Pasajero()
+            Usuario conductor1 = nuevo Conductor()
+            Usuario conductorPool1 = nuevo ConductorPool(maxPasajeros = 3)
             UcuRideShare rideShare = nuevo UcuRideShare()
             
-            rideShare.AddConductor(conductor1)
+            rideShare.Add(conductor1)
             Se publica en Twitter un nuevo conductor!
 
-            rideShare.AddConductor(conductorPool1)
+            rideShare.Add(conductorPool1)
             Se publica en Twitter un nuevo conductor!
             
-            rideShare.AddPasajero(pasajero1)
+            rideShare.Add(pasajero1)
             Se publica en Twitter nuevo registro de pasajero!
             
-            rideShare.AddPasajero(pasajero2)
+            rideShare.Add(pasajero2)
             Se publica en Twitter nuevo registro de pasajero!
 
-            rideShare.AddPasajero(pasajero3)
+            rideShare.Add(pasajero3)
             Se publica en Twitter nuevo registro de pasajero!
 
-            rideShare.CrearViaje(pasajero1) //Se crea un viaje con el pasajero 1 y el primer conductor disponible (conductor1)
-            rideShare.CrearViaje(pasajero2) //Se crea un viaje con el pasajero 2 y el primer conductor disponible (conductorPool1)
-            rideShare.CrearViaje(pasajero3) //Se crea un viaje con el pasajero 3 y el primer conductor disponible (conductorPool1, 
-                                                ya que es pool y admite mas de un pasajero)
             */
         }
     }
