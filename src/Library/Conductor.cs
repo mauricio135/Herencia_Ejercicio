009140330b9fced3989b10e7bd0f978f40115386
Nahuel Costa <nahuelcostadalto@hotmail.com>
2020-05-05 20:59:42 -0300

    Se agrega la clase Conductor

---
+++ src/Library/Conductor.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+
+namespace PII_Herencia
+{
+    public class Conductor:IPersona
+    {
+        public string Biografia{get;set;}
+        public string Vehiculo{get;set;}
+    }
+}
